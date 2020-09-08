c80b6a4146244a4e9463129f9df5d54ed79466a7
aroseca15 <aroseca15@gmail.com>
2020-09-07 18:25:04 -0700

    Testing new commit

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,13 +1,50 @@
 
 // Variables to control timer:
-const time = questions.length * 15; 
+const time = questions.length * 15;
 const questions = [
     {
-        question: "What is Javascript?", 
+        question: "What is Javascript?",
         choices: ["Programing Language", "Coffee Shop", "Name of Play", "Story Book"],
-        answer: "Programing Language" 
-    }, {}, {}, {}, {}, {}, {}
-]; 
+        answers: "Programing Language"
+    },
+
+    {
+        question: "HTML is considered the________ of a website..",
+        choices: ["Muscles", "Heart", "Skeleton", "Skin"],
+        answers: "Skeleton"
+    },
+
+    {
+        question: "What does 'document.getElementById' do?",
+        choices: ["Rename an element", "Create a div", "Runs in live server", "Grabs a specific element"],
+        answers: "Grabs a specific element"
+    },
+
+    {
+        question: "What does CSS do for a website?",
+        choices: ["Gives them internet access", "Makes them look appealing", "CSS is another name for HTML", "None of the above"],
+        answers: "Makes them look appealing"
+    },
+
+    {
+        question: "Which is the correct way to grab a class element in CSS?",
+        choices: ["?Class", "!Class", "9Class", "None of the above"],
+        answers: "None of the above"
+    },
+
+    {
+        question: "What syntax is this: '$()'?",
+        choices: ["Javascript", "JQuery", "HTML", "CSS"],
+        answers: "JQuery"
+    },
+
+    {
+        question: "Why is it good practice to keep JS and CSS separate from HTML?",
+        choices: ["Easier to debug", "Makes code look tidy", "Add new features easier ", "All the above"],
+        answers: "All the above"
+    } 
+];
+
 const timerEL = document.getElementById("time");
 const startBtn = document.getElementById("Start");
 // $(startBtn).on('click')
@@ -31,7 +68,7 @@ function clockTick() {
     time--;
     timerEL.textContent = time;
     // check if user ran out of time:
-    if (time <= 0){
+    if (time <= 0) {
         EndQuiz();
     }
 
