6dbb3dfb901a583ca6c84ddb819ad66fe592934f
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-08 23:05:46 -0700

    added javascript to start and shuffle questions. Questions show but choices do not of yet.

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,6 +1,6 @@
 
 // Variables to control timer:
-const time = questions.length * 15;
+// const time = questions.length * 15;
 const questions = [
     {
         question: "What is Javascript?",
@@ -45,34 +45,85 @@ const questions = [
     } 
 ];
 
-const timerEL = document.getElementById("time");
-const startBtn = document.getElementById("Start");
-// $(startBtn).on('click')
 
-let timerID;
+const startBtn = document.getElementById("start");
+startBtn.addEventListener('click' , startQuiz)
+let shuffledQuestions, currentQuestionIndex;
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerBtnsElement = document.getElementById('answer-buttons');
+
+
+
+function startQuiz(){
+console.log('started');
+startBtn.classList.add('hide');
+shuffledQuestions = questions.sort(()=> Math.random() - .5);
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide');
+setNextQuestion()
+}
+
+function setNextQuestion(){
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+questionElement.innerText = question.question;
+question.choices.forEach(answer => {
+  document.createElement('button');
+  button.innerText = choices.text;
+  button.classList.add('btn');
+  if (answer.correct){
+      button.dataset.correct = answer.correct
+  }
+  button.addEventListener('click' , selectAnswer);
+  answerButtonsElement.appendChild(button)
+})
+}
+
+function resetChoices(){
+    while (answerBtnsElement.firstChild){
+        answerBtnsElement.removeChild
+        (answerBtnsElement.firstChild)
+
+
+    }
+}
+
+function selectAnswer(e){
+
+
+}
+
+
+// const timerEL = document.getElementById("time");
+
+
+// let timerID;
 
 // Function to start quiz:
-startBtn.addEventListener('click', function StartQuiz() {
-    timerID = setInterval(clockTick, 1000);
-    timerEL.textContent = time;
-})
+// startBtn.addEventListener('click', function StartQuiz() {
+//     timerID = setInterval(clockTick, 1000);
+//     timerEL.textContent = time;
+// })
 
 
 // Function to Stop Quiz / Counter:
-function EndQuiz() {
-    clearInterval(timerID);
-}
+// function EndQuiz() {
+//     clearInterval(timerID);
+// }
 
-// Function for ClockTick:
-function clockTick() {
-    time--;
-    timerEL.textContent = time;
-    // check if user ran out of time:
-    if (time <= 0) {
-        EndQuiz();
-    }
+// // Function for ClockTick:
+// function clockTick() {
+//     time--;
+//     timerEL.textContent = time;
+//     // check if user ran out of time:
+//     if (time <= 0) {
+//         EndQuiz();
+//     }
 
-}
+// }
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Java_NotTheCoffee_Quiz
+Before I started this assignment I had a clear picture in my mind of what I wanted to do. For example, changing colors when the answer was "correct" or "incorrect". Or keeping score using one html page. To do that I used information from Web Dev Simplified(WDS) and Michael Dane (MD) on youtube to give me the spring board I needed. 
+
+I also worked with my tutors to create and check my javascript.
